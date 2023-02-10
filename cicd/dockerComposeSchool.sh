ad0ac5c8a7c700112c1bb9b79882074793c2a695
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2023-02-10 11:16:46 -0300

    chore: clean up the pipeline

--- cicd/dockerComposeSchool.sh
+++ cicd/dockerComposeSchool.sh
@@ -2,14 +2,8 @@
 
 echo "!!reminder: run this from root folder!!"
 
-docker compose build
-if [ $? -ne 0 ]; then    
-    exit -1
-fi
-echo "compose build: pass"
-
 echo "spinning up the environment"
-docker compose down && docker compose up
+docker compose build && docker compose down && docker compose up
 if [ $? -ne 0 ]; then    
     exit -1
 fi
